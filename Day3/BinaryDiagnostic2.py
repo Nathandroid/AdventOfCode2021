8af9f928ce5845531e108f723433a290580feaf7
MuffinMauler <NathanRobertson@dal.ca>
2021-12-07 19:05:36 -0400

    Completed Day 3 with recursive solution

--- Day3/BinaryDiagnostic2.py
+++ Day3/BinaryDiagnostic2.py
@@ -1,88 +1,72 @@
 import math
 
-report = open("input.txt")
+#determine the most common bit in the given list at the given position
+def mostCommonBit(values, position):
+	ones = 0 #how many bits '1' bits in the given position
+	for val in values:
+		if val[position] == '1':
+			ones += 1
 
-sums = [0] * 12 #https://stackoverflow.com/questions/8528178/list-of-zeros-in-python
-gamma = [0] * 12
-epsilon = [0] * 12
+	if ones >= len(values)/2:
+		return '1'
+	else:
+		return '0'
+
+#determine the least common bit in the given list at the given position
+def leastCommonBit(values, position):
+	zeros = 0 #how many bits '1' bits in the given position
+	for val in values:
+		if val[position] == '0':
+			zeros += 1
+
+	if zeros > len(values)/2:
+		return '1'
+	else:
+		return '0'
+
+#determine the oxygen rating recursively
+def oxygenRating(values, bitPosition):
+	if len(values) == 1: #if there is only one value left
+		return values
+
+	else:
+		newList = [] #for values that fit the unreasonable standards we've set for them go
+		mostCommon = mostCommonBit(values, bitPosition)
+
+		for val in values:
+			if val[bitPosition] == mostCommon: #if the bit matches our most common bit
+				newList.append(val) #add the value to our new list
+
+		return oxygenRating(newList, bitPosition + 1) #do it again with the new list and the next position
+
+#determine the oxygen rating recursively
+def CO2Rating(values, bitPosition):
+	if len(values) == 0:
+		stop
+	if len(values) == 1: #if there is only one value left
+		return values
+
+	else:
+		newList = [] #for values that fit the unreasonable standards we've set for them go
+		leastCommon = leastCommonBit(values, bitPosition)
+		
+		for val in values:
+			if val[bitPosition] == leastCommon: #if the bit matches our most common bit
+				newList.append(val) #add the value to our new list
+
+		return CO2Rating(newList, bitPosition + 1) #do it again with the new list and the next position
+
+
+report = open("input.txt")
 diagnostics = [] #the list to hold the inputs
 
-#get the sum of the number of 1s in each binary position
+#get the input into a list
 for diagnostic in report:
-	for bit in range(12):
-		sums[bit] += int(diagnostic[bit])
 	diagnostics.append(diagnostic.strip('\n'))
 
+oxygen = int(oxygenRating(diagnostics, 0)[0], 2)
+CO2 = int(CO2Rating(diagnostics, 0)[0], 2)
 
-
-mostCommon = [0] * 12
-
-for x in range(12):
-	if sums[x] < 500:
-		mostCommon[x] = '0'
-	elif sums[x] >= 500:
-		mostCommon[x] = '1'
-
-oxygenList = []
-CO2List = []
-
-#first pass through the list
-for diagnostic in diagnostics:
-	if mostCommon[0] == diagnostic[0]:
-		oxygenList.append(diagnostic)
-	else:
-		CO2List.append(diagnostic)
-
-'''At this point, we have two lists:
-	One list with all numbers beginning with the most common bit, 0
-	One list with all numbers beginning with the least common bit, 1 '''
-
-#first the oxygen list
-tempList = []
-
-for bit in range(12):
-	sums[bit] = 0
-
-for x in range(1, 12):
-
-	for num in oxygenList:#for every remaining number in the oxygen list
-		sums[x] += int(num[x]) #add the bit to sums
-
-	if sums[x] < (len(oxygenList)/2):
-		mostFrequent = '0'
-	else:
-		mostFrequent = '1'
-
-	for num in oxygenList:
-		if num[x] == mostFrequent:
-			tempList.append(num)
-
-	oxygenList = tempList.copy() #reset oxygenList
-	tempList = [] #clear temp list
-
-print(oxygenList)
-
-#now the CO2 list
-tempList = []
-
-for bit in range(12):
-	sums[bit] = 0
-
-for x in range(1, 12):
-
-	for num in CO2List:#for every remaining number in the CO2 list
-		sums[x] += int(num[x]) #add the bit to sums
-
-	if sums[x] > (len(CO2List)/2):
-		leastFrequent = '0'
-	else:
-		leastFrequent = '1'
-
-	for num in CO2List:
-		if num[x] == leastFrequent:
-			tempList.append(num)
-
-	CO2List = tempList.copy() #reset oxygenList
-	tempList = [] #clear temp list
-
-print(CO2List)
+print('Oxygen Generator rating: ', oxygen)
+print('CO2 Scrubber rating: ', oxygen)
+print('Life Support rating: ', oxygen * CO2)
